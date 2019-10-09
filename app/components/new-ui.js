1fc2aff8b4233904eebedeaa01311764bbac1d6b
RbMuhib <raghibbmuhib@gmail.com>
2019-10-09 13:51:27 +0600

    data parsing

--- app/components/new-ui.js
+++ app/components/new-ui.js
@@ -1,12 +1,39 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service'
+
 export default Component.extend({
 
       test: service('test-service'),
-
+      data: [],
       init() {
+
             this._super(...arguments);
             let userData = this.test.getData();
-            console.log(userData);
+            this.set('userData', userData);
+            this.loadData();
+            //console.log(userData);
+            console.log('init method');
       },
-});
+      loadData() {
+            console.log('load data');
+            let head = this.get('userData');
+            console.log(head);
+            for (let i = 0; i < head.length; i++) {
+
+                  let rowInfo = {
+                        id: head[i].id,
+                        name: head[i].name,
+                        username: head[i].username,
+                        email: head[i].email,
+                        address: head[i].address,
+                        phone: head[i].phone,
+                        website: head[i].website,
+                        company: head[i].company,
+                  }
+
+                  this.data.pushObject(rowInfo);
+            }
+            console.log(this.data);
+      }
+}
+);
